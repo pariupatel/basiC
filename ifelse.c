075c937a6da8d196a054dd82ac8e12c5b70b3118
Priyal Patel <priyal230906@gmail.com>
2024-08-30 03:13:56 +0530

    Simple If-else program

---
+++ ifelse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+
+{
+    int age;
+    printf("Enter your age: ");
+    scanf("%d",&age);
+
+    if(age>=18) {
+        printf("You are eligible for voting");
+    } else {
+        printf("You are not eligible for voting");
+    }
+}
