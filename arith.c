e23fcb072b24a792acc0ee2dc30bc144e3b07d4a
Priyal Patel <priyal230906@gmail.com>
2024-08-30 03:08:03 +0530

    Arithmetic operations on two numbers

---
+++ arith.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+int main()
+{
+    float a,b;
+    a = 32;
+    b = 6;
+
+    printf(" a + b = %f\n", a+b);
+    printf(" a - b = %f\n", a-b);
+    printf(" a * b = %f\n", a*b);
+    printf(" a / b = %f\n", a/b);
+
+    return 0;
+}
