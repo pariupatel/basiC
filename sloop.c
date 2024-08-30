dd96581c666a95bd93cd1c96256a71d58d744b0b
Priyal Patel <priyal230906@gmail.com>
2024-08-30 16:26:13 +0530

    Loop to Print a String for given no. of times

---
+++ sloop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+
+{
+    //loops
+    int a;
+    int i=1;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    while(i<=a){
+        printf("Hello World!\n");
+        i=i+1;
+    }
+
+}
