d2504403382c8cee22b6125b0dd0e804d9b1eb41
Priyal Patel <priyal230906@gmail.com>
2024-09-04 00:59:49 +0530

    User defined square function

---
+++ squarefunction.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int square(int n)
+{
+    return n*n;
+}
+
+int main()
+{
+    int a,r;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    r = square(a);
+    printf("Square of %d is %d",a,r);
+}
