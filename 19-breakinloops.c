8efc6f4e6ea155170bdf3ceaf990d410f707cee4
Priyal Patel <priyal230906@gmail.com>
2024-09-05 16:14:06 +0530

    Break statement

---
+++ 19-breakinloops.c
@@ -0,0 +1,15 @@
+#include <Stdio.h>
+int main()
+
+{
+    int i,age;
+    for (i=0;i<10;i++){
+        printf("%d\nEnter your age: ",i);
+        scanf("%d",&age);
+        if(age>10){
+            break;
+        }
+
+    }
+
+}
