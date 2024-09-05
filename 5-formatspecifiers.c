b8b3fbb3a63955c0b2cb4941c5472d1975cd9ee5
Priyal Patel <priyal230906@gmail.com>
2024-09-05 16:21:07 +0530

    Format specifiers in C

---
+++ 5-formatspecifiers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a = 8;
+    float b = 7.333;
+    printf("Hello world!\n");
+    printf("The value of a is %d and the value of b is %6.4f\n",a,b);
+    return 0;
+}
