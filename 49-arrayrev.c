38e4d380148faf40789372684a7b1af8d6818bce
Priyal Patel <priyal230906@gmail.com>
2024-10-23 21:53:39 -0700

    Create 49-arrayrev.c

---
+++ 49-arrayrev.c
@@ -0,0 +1,15 @@
+//array reversal
+
+#include <stdio.h>
+void arrayreverse(int array[])
+{
+    return 0;
+}
+int main() {
+    int a[]={1,2,3,4,5};
+    for (int i=0; i<5; i++) {
+        printf("the value of element %d is %d\n", a[i],a[i]);
+        
+    }
+    return 0;
+}
